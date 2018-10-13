708b5f8b0fe8cf2d5a10d1dfd1652a0bbaa1aee2
ZeeRooo <chavesjuan400@gmail.com>
2018-10-12 22:05:37 -0300

    Hello Pie

--- init/Android.mk
+++ init/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
-LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_CPPFLAGS := -Wall
 LOCAL_SRC_FILES := init_surnia.cpp
 LOCAL_MODULE := libinit_surnia
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
